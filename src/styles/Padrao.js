acb7b60c83ad63f8836ae57eeeb31701016fca2c
Vanessa Silva <vanessa.silva@compasso.com.br>
2020-03-04 10:18:13 -0300

    Init
    Init

---
+++ src/styles/Padrao.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    ex: {
+      paddingHorizontal: 15,
+      marginVertical: 5,
+      borderRadius: 10,
+      borderColor: '#222',
+      fontSize: 24,
+      fontWeight: 'bold'
+    }
+  });
